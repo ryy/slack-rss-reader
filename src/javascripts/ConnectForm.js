4e6ef346ee9f08bd048e035895215f83f043cfb3
ryo.naruse <naruse19910801@gmail.com>
2021-01-12 07:13:50 +0900

    Add how to connect link

--- src/javascripts/ConnectForm.js
+++ src/javascripts/ConnectForm.js
@@ -13,8 +13,8 @@ Vue.component('connect-form', {
     '</div>' +
     '<div class="mb-6">' +
       '<label class="block text-gray-700 text-sm font-bold mb-2" for="channel">Channel</label>' +
-      '<input v-model="channel" class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="channel" type="text" placeholder="channel">' +
-      '<p class="text-red-500 text-xs italic">Please choose a password.</p>' +
+      '<input v-model="channel" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="channel" type="text" placeholder="channel">' +
+      '<p class="text-gray-700 text-xs italic"><a href="https://gist.github.com/ryy/58605d2c8f786920519ee6ed57653699" target="_blank">How to connect</a></p>' +
     '</div>' +
     '<div class="text-right mb-6">' +
       '<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 text-right" @click="connect">' +
